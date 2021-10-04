732864b2b009b810bb4acc702ec3fe2c1d53dbff
nischay2356 <84444874+nischay2356@users.noreply.github.com>
2021-10-04 09:09:54 +0530

    Add files via upload

---
+++ index.html.java
@@ -0,0 +1,62 @@
+﻿Nischay P
+Student
+* Details
+* Education
+* Technical Skills
+* Project
+Details about me
+
+
+Name: Nischay P
+Age: 22
+Gender: Male
+Email: nischay17p@gmail.com
+Phone_no: 7019795022
+Education
+
+
+
+
+
+
+Degree
+	Institute
+	University
+	Percentage
+	Secondary Education
+	Assumption English High School
+	KSEEB
+	88.4
+	Senior Secondary (Pre- Engineering)
+	Vidya Mandir Ind Pu College
+	Government of Karnataka
+	79.3
+	B.Tech TCE 
+	Kammavari Sangha Institute Of Technology
+	VTU
+	58.3
+	
+
+
+
+
+
+
+
+
+
+
+
+Technical Skills
+* Java
+* HTML5
+* CSS
+* JavaScript
+* MySQL
+* Python
+Project
+
+
+Role: Team member
+Title: Media Forensics and deep fakes
+Description:In this project we had built an entire working model to detect if the uploaded image is a real or a Fake image. This model consisted of two parts : The Front-end Android application and Back-end python code. In the front-end the Android application is useful to upload the image and transfer it to the back-end of code. In Back-end the image is made to run through the python code using Error- Level Analysis and Convolution Neural Network to obtain the results.
